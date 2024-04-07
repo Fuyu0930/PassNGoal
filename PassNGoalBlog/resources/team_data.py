c919e921ddaf0983069bf17717d2ac79a9c2b710
Fuyu0930 <fuyuwang2000@gmail.com>
2024-04-06 23:48:55 -0400

    Created TeamSquadData API to get certain team's current squad

--- PassNGoalBlog/resources/team_data.py
+++ PassNGoalBlog/resources/team_data.py
@@ -16,3 +16,32 @@ api_host = headers["X-RapidAPI-Host"]
 ######## Set up team API ########
 #################################
 
+class TeamSquadData(Resource):
+    def get(self, team_id):
+        querystring = {"team": team_id}
+        headers = {"X-RapidAPI-Key": api_key,
+                   "X-RapidAPI-Host": api_host}
+        response = requests.get("https://api-football-v1.p.rapidapi.com/v3/players/squads", 
+                                headers=headers, 
+                                params=querystring)
+        
+        # Parse the json and get the expected result
+        data = response.json()
+        result = {"Goalkeeper": [],
+                  "Defender": [],
+                  "Midfielder": [],
+                  "Attacker":[]}
+        players = data["response"][0]["players"]
+
+        # Sort the player based on their position
+        for player in players:
+            player_json = {
+                "id": player["id"],
+                "name": player["name"],
+                "age": player["age"],
+                "number": player["number"],
+                "photo": player["photo"]
+            }
+            result[player["position"]].append(player_json)
+
+        return result
