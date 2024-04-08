1f51fac4cf05529d07d566e84e9337709b18180d
Fuyu0930 <fuyuwang2000@gmail.com>
2024-04-08 00:49:42 -0400

    Add team current standing in team overview views

--- PassNGoalBlog/teams/views.py
+++ PassNGoalBlog/teams/views.py
@@ -5,13 +5,30 @@ from PassNGoalBlog.resources.team_data import TeamPastFixtures
 from PassNGoalBlog.resources.team_data import TeamCurrentFixture 
 from PassNGoalBlog.resources.team_data import TeamLeagueInfo
 from PassNGoalBlog.resources.team_data import TeamStatistics
-
+from PassNGoalBlog.resources.league_data import StandingData, TopScorersData, TopAssistData
 
 teams = Blueprint('teams', __name__)
+
+def find_target_team(data, target):
+    index = None
+    
+    for i, team_data in enumerate(data):
+        if team_data["team_id"] == target:
+            index = i
+            break
+
+    if index == 0:
+        return data[:3]
+    elif index == len(data) - 1:
+        return data[:-3]
+    else:
+        return data[index-1:index+2]
+
 @teams.route('/team_overview/<int:year>/<int:team_id>/<int:is_current>')
 def team_overview(year, team_id, is_current):
     team_league_resource = TeamLeagueInfo()
     team_statistics_resource = TeamStatistics()
+    standing_data_resource = StandingData()
 
     if is_current == 1:
         team_league_data = team_league_resource.get(year, team_id, 1)
@@ -20,6 +37,11 @@ def team_overview(year, team_id, is_current):
 
     league_id = team_league_data["league_id"]
 
+    # Get the standing with the target team
+    league_standing_data = standing_data_resource.get(year, league_id)
+    team_standing = find_target_team(league_standing_data, team_id)
+
+    # Get the team statistics
     team_overview_data = team_statistics_resource.get(year, team_id, league_id)
 
     league_data = team_overview_data["league"]
@@ -42,7 +64,10 @@ def team_overview(year, team_id, is_current):
                            draw_fixtures=draw_fixtures,
                            lose_fixtures=lose_fixtures,
                            goals_for=goals_for,
-                           goals_against=goals_against)
+                           goals_against=goals_against,
+                           team_standing=team_standing)
+
+
 
 @teams.route('/team_squad/<int:team_id>')
 def team_squad(team_id):
